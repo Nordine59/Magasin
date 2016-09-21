740a094b5a17fdb5714b40421f04b6424c9bf8f6
Nordine59 <itounour@hotmail.fr>
2016-09-21 11:05:51 +0200

    Deuxieme version  Streaming

---
+++ src/magasin/entities/Adresse.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magasin.entities;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author admin
+ */
+@Embeddable
+public class Adresse {
+    
+    private String numeroRue;
+    
+    private String rue;
+    
+    private String ville;
+    
+    private String cp;
+    
+    private String pays;
+    
+            
+    
+    
+    
+    
+    
+    
+}
---
+++ test/Magasin/test/MagasinTEST.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Magasin.test;
+
+import javax.persistence.Persistence;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author admin
+ */
+public class MagasinTEST {
+    
+    
+    @Test
+    public void test(){
+        Persistence.createEntityManagerFactory("PU");
+        
+        
+        
+        
+    }
+    
+}
