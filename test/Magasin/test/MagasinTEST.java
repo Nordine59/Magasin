898322f2e92018e695c0906a9dd324e4f7793b3f
Nordine59 <itounour@hotmail.fr>
2016-09-22 10:40:35 +0200

    Derniere version

--- test/Magasin/test/MagasinTEST.java
+++ test/Magasin/test/MagasinTEST.java
@@ -7,10 +7,13 @@ package Magasin.test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import magasin.entities.Categorie;
 import magasin.entities.Client;
 import magasin.entities.Commande;
 import magasin.entities.Produit;
+import static org.junit.Assert.fail;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -21,7 +24,7 @@ public class MagasinTEST {
 
     //creation test
     // @Test
-    @Test
+    //@Test
     public void testliste() {
 
         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
@@ -37,25 +40,42 @@ public class MagasinTEST {
         }
 
     }
-}
-    public void test() {
 
-        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+    @Before
+    public void avant() {
+
+        //vide toutes les tables
+         EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+         em.getTransaction().begin();
+         Query query = em.createQuery("DELETE FROM Produit p");
+         query.executeUpdate();
+         em.createQuery("DELETE FROM Categorie c").executeUpdate();
+        
+         em.getTransaction().commit();
+        
+        //ajoute des donnes en spécifiant les IDS que l on recup ds les tests unitaires
+        
+        
+       
 
         //demarre transaction
-        em.getTransaction().begin();
+       
 
         //Persister en base des données
+        em.getTransaction().begin();
         Categorie c1 = new Categorie();
+        c1.setId(1L);
         c1.setNom("Basket");
         //commande pour inserer c1
         em.persist(c1);
 
         Categorie c2 = new Categorie();
+        c2.setId(2L);
         c2.setNom("Lunettes de soleil");
         em.persist(c2);
 
         Produit rayBan = new Produit();
+        rayBan.setId(3L);
         rayBan.setCategorie(c2);
         rayBan.setTitre("rayBan");
         em.persist(rayBan);
@@ -65,3 +85,17 @@ public class MagasinTEST {
         em.getTransaction().commit();
 
     }
+
+    @Test
+    public void verifiQueCatIdEstBasket() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        Categorie cat = em.find(Categorie.class, 1L);
+
+        if (cat.getNom().equals("Basket") == false) {
+            fail("MARCHE PAS MEC");
+            
+        }
+ 
+    }
+
+}
